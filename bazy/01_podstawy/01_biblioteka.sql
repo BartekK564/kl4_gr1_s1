9406317ccec3907df22eb9164bc89b9efff62a23
Bartosz Król <redm05074@gmail.com>
2025-09-08 12:43:41 +0200

    5.09

--- bazy/01_podstawy/01_biblioteka.sql
+++ bazy/01_podstawy/01_biblioteka.sql
@@ -1,2 +1,26 @@
 -- start
 show databases;
+mysql -u root;
+CREATE DATABASE 4e_1_biblioteka; 
+SHOW databases;
+USE 4e_1_biblioteka;
+CREATE TABLE ksiazka(
+    tytul varchar(255),
+    autor varchar(255),
+    cena int
+);
+SHOW TABLES;
+DESC ksiazka;
+INSERT INTO ksiazka
+VALUES
+('Harry Potter','J.K Rowling',25);
+SELECT * FROM ksiazka;
+
+INSERT INTO ksiazka
+VALUES
+('Lalak','Boleslaw Prus',50);
+DELETE FROM ksiazka
+WHERE tytul = 'Harry Potter';
+
+ALTER TABLE ksiazka
+ADD id int primary key AUTO_INCREMENT;
