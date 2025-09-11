f38b405cb5a7dafd761071f97d5d8c96f2d5780e
Bartosz Król <redm05074@gmail.com>
2025-09-11 09:34:37 +0200

    11.09

--- bazy/01_podstawy/01_biblioteka.sql
+++ bazy/01_podstawy/01_biblioteka.sql
@@ -24,3 +24,37 @@ WHERE tytul = 'Harry Potter';
 
 ALTER TABLE ksiazka
 ADD id int primary key AUTO_INCREMENT;
+DELETE FROM ksiazka
+WHERE id=1;
+CREATE TABLE gatunki(
+    id int primary key AUTO_INCREMENT,
+    nazwa varchar(100)
+);
+ALTER TABLE ksiazka
+ADD gatunek int;
+
+INSERT INTO gatunki
+(nazwa)
+VALUES
+('dramat'),
+('komedia');
+
+INSERT INTO ksiazka
+(tytul,autor,gatunek)
+VALUES
+('Harry Potter','J.K. Rowling',3);
+
+CREATE DATABASE 4e_1_wizyty;
+USE 4e_1_wizyty;
+
+CREATE TABLE wizyty(
+    imie varchar(100),
+    nazwisko varchar(100),
+    telefon varchar(100),
+    data_wizyty date,
+    typ varchar(100)
+);
+
+INSERT INTO wizyty
+VALUES
+('Jan','Kowalski','21134212','2000-12-9','leczenie'); 
